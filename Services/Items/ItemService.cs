cc61d64f26c1f268550bf650936e5e0ff21c31e6
Serana Wilger <sbwilger@gmail.com>
2020-07-02 20:53:17 -0400

    Add files via upload

---
+++ Services/Items/ItemService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using sbwilger.DAL;
+using sbwilger.DAL.Models.Items;
+
+namespace sbwilger.Core.Services.Items
+{
+    public interface IItemService
+    {
+        Task<Item> GetItemByName(string itemName);
+    }
+
+    public class ItemService : IItemService
+    {
+        private readonly RPGContext _context;
+
+        public ItemService(RPGContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Item> GetItemByName(string itemName)
+        {
+            itemName = itemName.ToLower();
+
+            return await _context.Items.FirstOrDefaultAsync(x => x.Name.ToLower() == itemName).ConfigureAwait(false);
+        }
+
+    }
+}
