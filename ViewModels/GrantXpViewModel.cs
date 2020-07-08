780018ecfcdf515d6f0e102613e59204a0b838c0
Serana Wilger <sbwilger@gmail.com>
2020-07-07 22:06:26 -0400

    Add files via upload

---
+++ ViewModels/GrantXpViewModel.cs
@@ -0,0 +1,13 @@
+﻿using sbwilger.DAL.Models.Profiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sbwilger.Core.ViewModels
+{
+    public class GrantXpViewModel
+    {
+        public Profile Profile;
+        public bool LevelledUp;
+    }
+}
